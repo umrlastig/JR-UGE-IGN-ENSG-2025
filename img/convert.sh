f3ed0b64909bc5c386f5231601fa5fd102439187
Juste Raimbault <juste.raimbault@polytechnique.edu>
2025-03-10 21:38:28 +0100

    adjust_title_slide

--- img/convert.sh
+++ img/convert.sh
@@ -1,4 +1,4 @@
 
 convert -density 1000 JR2025_title.pdf -resize 2000x -quality 100 JR2025_title.jpg
 
-convert -density 1000 JR2025.pdf -resize 2000x -quality 100 JR2025.jpg
+#convert -density 1000 JR2025.pdf -resize 2000x -quality 100 JR2025.jpg
